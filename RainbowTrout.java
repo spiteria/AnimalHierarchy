3e43a4e9d7aee1914a2a04402079051081cabde9
spiteria <spiteria@gmail.com>
2015-01-15 01:47:13 -0500

    RainbowTrout class
    Java file

---
+++ RainbowTrout.java
@@ -0,0 +1,8 @@
+package AnimalHierarchy;
+
+public class RainbowTrout extends Fish{
+	public RainbowTrout(){
+		super("Rainbow Trout", "It has bands of brilliant speckled multicolored stripes running"+
+				" nearly the whole length of its body");
+	}
+}
